88a94b7785319c638e773feb0cf6e44892d3f418
noemi <noemi@DESKTOP-AO3FNSD>
2021-12-08 11:24:23 +0100

    Dodaj pliki projektów.

---
+++ DrinkRecipeBook/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DrinkRecipeBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to our app. You will find great recipes for drinks in it.");
+            Console.WriteLine("Please let me know what you want to do:");
+            Console.WriteLine("1. Add Recipe");
+            Console.WriteLine("2. Edit Recipe");
+            Console.WriteLine("3. Find Recipe");
+            Console.WriteLine("4. Delete Recipe");
+
+            string choice = Console.ReadLine();
+
+            Console.WriteLine($"You have chosen option number: {choice}");
+        }
+    }
+}
